d2e146c29c55897d443387d4090643868053e02d
Garrett Young <garrettyoung510@gmail.com>
2019-09-24 22:42:47 -0700

    added date and datefns

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const clock = document.querySelector(".clock");
+const date = document.querySelector('.date');
 
 const tick = () => {
   const now = new Date();
@@ -7,16 +8,34 @@ const tick = () => {
   const m = now.getMinutes();
   const s = now.getSeconds();
 
-  console.log(h, m, s);
-  const html = `
-    <span>${h}</span> :
+  const d = dateFns.format(now, 'dddd, Do, MMMM, YYYY')
+  //   console.log(h, m, s);
+  const time = `
+  <span>${h}</span> :
     <span>${m}</span> :
     <span>${s}</span>
     `;
 
-    clock.innerHTML = html;
+    clock.innerHTML = time;
 
+    const today = `<div>${d}</div>`;
+    date.innerHTML = today;
 };
 
 // every second it will tick
 setInterval(tick, 1000);
+
+
+// // date FNS
+// const now = new Date();
+// console.log(dateFns.isToday(now));
+
+// // formatting option
+// console.log(dateFns.format(now, 'YYYY'));
+// console.log(dateFns.format(now, 'MMM'));
+// console.log(dateFns.format(now, 'dddd'));
+// console.log(dateFns.format(now, 'Do'));
+// console.log(dateFns.format(now, 'dddd, Do, MMMM, YYYY'));
+
+// const before = new Date('February 1 2019 12:00:00');
+// console.log(dateFns.distanceInWords(now, before, {addSuffix: true}));
